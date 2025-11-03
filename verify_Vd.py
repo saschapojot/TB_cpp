4ff47a2ed58703b4814301fc9b38568a20e608d2
Xi LIU <xi.liu@u.nus.edu>
2025-11-03 17:30:34 +0800

    Vd

--- verify_Vd.py
+++ verify_Vd.py
@@ -21,14 +21,29 @@ random_matrix = np.random.randn(3, 3)
 R_numpy, _ = np.linalg.qr(random_matrix)
 
 # Convert to SymPy
-r00, r01, r02 = R_numpy[0, :]
-r10, r11, r12 = R_numpy[1, :]
-r20, r21, r22 = R_numpy[2, :]
+# r00, r01, r02 = R_numpy[0, :]
+# r10, r11, r12 = R_numpy[1, :]
+# r20, r21, r22 = R_numpy[2, :]
 
 # Convert to SymPy for symbolic computation
 # r00, r01, r02 = Float(r00), Float(r01), Float(r02)
 # r10, r11, r12 = Float(r10), Float(r11), Float(r12)
 # r20, r21, r22 = Float(r20), Float(r21), Float(r22)
+
+
+
+alpha=pi/3
+r00=cos(alpha)
+r01=-sin(alpha)
+r10=sin(alpha)
+r11=cos(alpha)
+r22=1
+
+r02=0
+r12=0
+r20=0
+r21=0
+
 
 N0=sqrt(15/(4*pi))
 
@@ -84,6 +99,52 @@ Vd=Matrix([
     [u30,u31,u32,u33,u34],
     [u40,u41,u42,u43,u44]
 ])
+pprint(Vd)
+# print("Vd * Vd.T:")
+# pprint((Vd * Vd.T))
 
-print("Vd * Vd.T:")
-pprint((Vd * Vd.T).evalf())
+
+def GetSymD(R):
+    R_11, R_12, R_13 = R[0,0], R[0,1], R[0,2]
+    R_21, R_22, R_23 = R[1,0], R[1,1], R[1,2]
+    R_31, R_32, R_33 = R[2,0], R[2,1], R[2,2]
+    RD = zeros(5,5)
+    sr3 = sqrt(3)
+    #
+    RD[0,0] = R_11*R_22+R_12*R_21
+    RD[0,1] = R_21*R_32+R_22*R_31
+    RD[0,2] = R_11*R_32+R_12*R_31
+    RD[0,3] = 2*R_11*R_12+R_31*R_32
+    RD[0,4] = sr3*R_31*R_32
+    #
+    RD[1,0] = R_12*R_23+R_13*R_22
+    RD[1,1] = R_22*R_33+R_23*R_32
+    RD[1,2] = R_12*R_33+R_13*R_32
+    RD[1,3] = 2*R_12*R_13+R_32*R_33
+    RD[1,4] = sr3*R_32*R_33
+    #
+    RD[2,0] = R_11*R_23+R_13*R_21
+    RD[2,1] = R_21*R_33+R_23*R_31
+    RD[2,2] = R_11*R_33+R_13*R_31
+    RD[2,3] = 2*R_11*R_13+R_31*R_33
+    RD[2,4] = sr3*R_31*R_33
+    #
+    RD[3,0] = R_11*R_21-R_12*R_22
+    RD[3,1] = R_21*R_31-R_22*R_32
+    RD[3,2] = R_11*R_31-R_12*R_32
+    RD[3,3] = (R_11**2-R_12**2 )+1/2*(R_31**2-R_32**2 )
+    RD[3,4] = sr3/2*(R_31**2-R_32**2 )
+    #
+    RD[4,0] = 1/sr3*(2*R_13*R_23-R_11*R_21-R_12*R_22)
+    RD[4,1] = 1/sr3*(2*R_23*R_33-R_21*R_31-R_22*R_32)
+    RD[4,2] = 1/sr3*(2*R_13*R_33-R_11*R_31-R_12*R_32)
+    RD[4,3] = 1/sr3*(2*R_13**2-R_11**2-R_12**2 )+1/sr3/2*(2*R_33**2-R_31**2-R_32**2 )
+    RD[4,4] = 1/2*(2*R_33**2-R_31**2-R_32**2 )
+
+    return RD.T
+R=Matrix([[r00,r01,r02],
+          [r10,r11,r12],
+          [r20,r21,r22]
+          ])
+
+pprint(GetSymD(R))
